12e1156a9442014951416cecd2cc791aef11648e
Miles on Film <milesonfilm@gmail.com>
2026-04-22 21:24:27 +0530

    first commit

---
+++ my-pwa/app/src/main/java/app/hosted/asia_southeast1/securevault__vault_84ff7/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.hosted.asia_southeast1.securevault__vault_84ff7.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
